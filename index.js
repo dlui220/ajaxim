17b97f5847e69d09a299e539b7599fdd3323d062
stinographer <pacqqu@gmail.com>
2016-05-27 12:25:11 -0400

    updated

--- index.js
+++ index.js
@@ -3,19 +3,22 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-		res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-			io.emit('chat message', msg);
+    io.emit('chat message', msg);
   });
 });
 
 io.on('connection', function(socket){
-		console.log('a user connected');
-});
-
-http.listen(3000, function(){
-		console.log('listening on *:3000');
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
